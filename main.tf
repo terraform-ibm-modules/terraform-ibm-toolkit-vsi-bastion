730c597b14bf60d7381660cb3e3723531b0dc0b9
Sean Sundberg <seansund@us.ibm.com>
2021-03-25 00:59:38 -0500

    Sets terraform version
    Signed-off-by: Sean Sundberg <seansund@us.ibm.com>

--- main.tf
+++ main.tf
@@ -1,3 +1,9 @@
+provider "ibm" {
+  ibmcloud_api_key = var.ibmcloud_api_key
+  region           = var.region
+  generation       = 2
+}
+
 data ibm_resource_group group {
   name = var.resource_group_name
 }
---
+++ version.tf
@@ -0,0 +1,10 @@
+terraform {
+  required_version = ">= 0.13.2"
+
+  required_providers {
+    ibm = {
+      source = "IBM-Cloud/ibm"
+      version = ">= 1.17"
+    }
+  }
+}
