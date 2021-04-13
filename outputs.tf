0b3e1640d16bb1fc2fc92303c8b1b9334f50e6f7
Sean Sundberg <seansund@us.ibm.com>
2021-04-13 01:58:56 -0500

    Updates output variables
    Signed-off-by: Sean Sundberg <seansund@us.ibm.com>

--- outputs.tf
+++ outputs.tf
@@ -2,16 +2,10 @@ output vpc_name {
   value = data.ibm_is_vpc.vpc.name
 }
 
-output bastion_ip {
-  value = module.bastion.bastion_public_ip
+output bastion_ips {
+  value = module.bastion[*].bastion_public_ip
 }
 
-output bastion_private_ip {
-  value = module.bastion.bastion_private_ip
-}
-
-output instance_ips {
-  value = [
-    for instance in local.instances : instance.primary_network_interface.0.primary_ipv4_address
-  ]
+output bastion_private_ips {
+  value = module.bastion[*].bastion_private_ip
 }
--- test/stages/stage2-bastion.tf
+++ test/stages/stage2-bastion.tf
@@ -6,7 +6,7 @@ module "bastion" {
   name_prefix         = var.name_prefix
   ibmcloud_api_key    = var.ibmcloud_api_key
   vpc_name            = module.vpc.name
-  subnet_count        = module.subnets.subnet_count
+  subnet_count        = module.subnets.count
   subnets             = module.subnets.subnets
   ssh_key_id          = module.vpcssh.id
 }
