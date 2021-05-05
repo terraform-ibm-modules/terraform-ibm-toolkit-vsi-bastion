2537f336c0ffe996a443e2505107de0fb4b0a0e8
Sean Sundberg <seansund@us.ibm.com>
2021-05-05 11:26:23 -0500

    Allows ssh only from internal networks by default (#8)
    * Allows ssh only from internal networks by default
    * Rebuilds module to use ibm-vpc-vsi module
    
    Signed-off-by: Sean Sundberg <seansund@us.ibm.com>

--- test/stages/stage2-bastion.tf
+++ test/stages/stage2-bastion.tf
@@ -1,12 +1,11 @@
 module "bastion" {
   source = "./module"
 
-  resource_group_name = module.resource_group.name
+  resource_group_id = module.resource_group.id
   region              = var.region
-  name_prefix         = var.name_prefix
   ibmcloud_api_key    = var.ibmcloud_api_key
   vpc_name            = module.vpc.name
-  subnet_count        = module.subnets.count
-  subnets             = module.subnets.subnets
+  vpc_subnet_count    = module.subnets.count
+  vpc_subnets         = module.subnets.subnets
   ssh_key_id          = module.vpcssh.id
 }
--- variables.tf
+++ variables.tf
@@ -1,6 +1,6 @@
-variable "resource_group_name" {
+variable "resource_group_id" {
   type        = string
-  description = "The name of the IBM Cloud resource group where the Bastion instance will be created."
+  description = "The id of the IBM Cloud resource group where the VPC has been provisioned."
 }
 
 variable "region" {
@@ -8,45 +8,156 @@ variable "region" {
   description = "The IBM Cloud region where the cluster will be/has been installed."
 }
 
-variable "name_prefix" {
-  type        = string
-  description = "The name of the vpc resource"
-  default     = ""
-}
-
 variable "ibmcloud_api_key" {
   type        = string
   description = "The IBM Cloud api token"
 }
 
-variable "tags" {
-  type        = list(string)
-  description = "List of tags"
-  default     = []
-}
-
 variable "vpc_name" {
   type        = string
-  description = "The name of the existing VPC instance"
+  description = "The name of the vpc instance"
 }
 
-variable "subnet_count" {
+variable "label" {
+  type        = string
+  description = "The label for the server instance"
+  default     = "server"
+}
+
+variable "image_name" {
+  type        = string
+  description = "The name of the image to use for the virtual server"
+  default     = "ibm-ubuntu-18-04-1-minimal-amd64-2"
+}
+
+variable "vpc_subnet_count" {
   type        = number
-  description = "The number of subnets on the vpc instance"
+  description = "Number of vpc subnets"
 }
 
-variable "subnets" {
-  type        = list(object({id = string, zone = string, label = string}))
-  description = "The list of subnet objects where bastion servers will be provisioned"
+variable "vpc_subnets" {
+  type        = list(object({
+    label = string
+    id    = string
+    zone  = string
+  }))
+  description = "List of subnets with labels"
+}
+
+variable "profile_name" {
+  type        = string
+  description = "Instance profile to use for the bastion instance"
+  default     = "cx2-2x4"
 }
 
 variable "ssh_key_id" {
   type        = string
-  description = "The id of a key registered with the VPC"
+  description = "SSH key ID to inject into the virtual server instance"
+}
+
+variable "allow_ssh_from" {
+  type        = string
+  description = "An IP address, a CIDR block, or a single security group identifier to allow incoming SSH connection to the virtual server"
+  default     = "10.0.0.0/8"
 }
 
 variable "create_public_ip" {
   type        = bool
+  description = "Set whether to allocate a public IP address for the virtual server instance"
   default     = false
-  description = "Flag to create a public ip address on the bastion instance"
+}
+
+variable "init_script" {
+  type        = string
+  default     = ""
+  description = "Script to run during the instance initialization. Defaults to an Ubuntu specific script when set to empty"
+}
+
+variable "tags" {
+  type        = list(string)
+  default     = []
+  description = "Tags that should be added to the instance"
+}
+
+variable "flow_log_cos_bucket_name" {
+  type        = string
+  description = "Cloud Object Storage bucket id for flow logs (optional)"
+  default     = ""
+}
+
+variable "kms_enabled" {
+  type        = bool
+  description = "Flag indicating that the volumes should be encrypted using a KMS."
+  default     = false
+}
+
+variable "kms_key_crn" {
+  type        = string
+  description = "The crn of the root key in the kms instance. Required if kms_enabled is true"
+  default     = null
+}
+
+variable "auto_delete_volume" {
+  type        = bool
+  description = "Flag indicating that any attached volumes should be deleted when the instance is deleted"
+  default     = true
+}
+
+variable "security_group_rules" {
+  # type = list(object({
+  #   name=string,
+  #   direction=string,
+  #   remote=optional(string),
+  #   ip_version=optional(string),
+  #   tcp=optional(object({
+  #     port_min=number,
+  #     port_max=number
+  #   })),
+  #   udp=optional(object({
+  #     port_min=number,
+  #     port_max=number
+  #   })),
+  #   icmp=optional(object({
+  #     type=number,
+  #     code=optional(number)
+  #   })),
+  # }))
+  description = "List of security group rules to set on the bastion security group in addition to the SSH rules"
+  default = [
+    {
+      name      = "http_outbound"
+      direction = "outbound"
+      remote    = "0.0.0.0/0"
+      tcp = {
+        port_min = 80
+        port_max = 80
+      }
+    },
+    {
+      name      = "https_outbound"
+      direction = "outbound"
+      remote    = "0.0.0.0/0"
+      tcp = {
+        port_min = 443
+        port_max = 443
+      }
+    },
+    {
+      name      = "dns_outbound"
+      direction = "outbound"
+      remote    = "0.0.0.0/0"
+      udp = {
+        port_min = 53
+        port_max = 53
+      }
+    },
+    {
+      name      = "icmp_outbound"
+      direction = "outbound"
+      remote    = "0.0.0.0/0"
+      icmp = {
+        type = 8
+      }
+    }
+  ]
 }
