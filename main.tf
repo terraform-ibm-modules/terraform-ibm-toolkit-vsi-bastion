24ca7f4fb3805e01c63f607e2770da7bb251b248
Sean Sundberg <seansund@us.ibm.com>
2021-05-13 00:02:34 -0500

    Bumps vpc-vsi module to v1.6.0
    Signed-off-by: Sean Sundberg <seansund@us.ibm.com>

--- main.tf
+++ main.tf
@@ -28,7 +28,7 @@ data ibm_is_subnet vpc_subnet {
 }
 
 module "vsi-instance" {
-  source = "github.com/cloud-native-toolkit/terraform-ibm-vpc-vsi.git?ref=v1.5.2"
+  source = "github.com/cloud-native-toolkit/terraform-ibm-vpc-vsi.git?ref=v1.6.0"
 
   resource_group_id    = var.resource_group_id
   region               = var.region
