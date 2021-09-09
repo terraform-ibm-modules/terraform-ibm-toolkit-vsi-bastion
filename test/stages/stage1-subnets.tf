7e8a6e59fc3a97cf322f4b198434918613da2405
Sean Sundberg <seansund@us.ibm.com>
2021-09-09 12:22:48 -0500

    Opens up ACL to port 22 for test
    Signed-off-by: Sean Sundberg <seansund@us.ibm.com>

--- test/stages/stage1-subnets.tf
+++ test/stages/stage1-subnets.tf
@@ -8,4 +8,17 @@ module "subnets" {
   gateways          = module.gateways.gateways
   _count            = 1
   label             = "bastion"
+  acl_rules         = [{
+    name="inbound-ssh"
+    action="allow"
+    direction="inbound"
+    source="0.0.0.0/0"
+    destination="0.0.0.0/0"
+    tcp = {
+      port_min = 22
+      port_max = 22
+      source_port_min = 22
+      source_port_max = 22
+    }
+  }]
 }
