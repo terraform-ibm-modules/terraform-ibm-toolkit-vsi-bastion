fe448f5667173c3fdfc9be9336c0f41b19a1a1f8
Sean Sundberg <seansund@us.ibm.com>
2021-10-15 16:36:32 -0500

    Reduces permissions for private key file
    Signed-off-by: Sean Sundberg <seansund@us.ibm.com>

--- test/stages/stage1-vpc-ssh.tf
+++ test/stages/stage1-vpc-ssh.tf
@@ -11,6 +11,6 @@ module "vpcssh" {
 
 resource null_resource write_private_key {
   provisioner "local-exec" {
-    command = "echo '${module.vpcssh.private_key}' > .private-key"
+    command = "echo '${module.vpcssh.private_key}' > .private-key && chmod 700 .private-key"
   }
 }
