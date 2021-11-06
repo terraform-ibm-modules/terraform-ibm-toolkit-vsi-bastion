947a9b9e1b391a74f9ac7594e3b659d2a3be58d8
Sean Sundberg <seansund@us.ibm.com>
2021-11-06 15:17:38 -0500

    Updates test variables
    Signed-off-by: Sean Sundberg <seansund@us.ibm.com>

--- test/stages/stage1-vpc-ssh.tf
+++ test/stages/stage1-vpc-ssh.tf
@@ -2,7 +2,6 @@ module "vpcssh" {
   source = "github.com/cloud-native-toolkit/terraform-ibm-vpc-ssh.git"
 
   resource_group_name = module.resource_group.name
-  region              = var.region
   name_prefix         = var.name_prefix
   public_key          = ""
   private_key         = ""
