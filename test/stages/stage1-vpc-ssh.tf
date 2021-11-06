3fb24c886e17602eeef29251e9dd068d7024d01b
Sean Sundberg <seansund@us.ibm.com>
2021-11-06 10:32:08 -0500

    Updates handling of provider config
    Signed-off-by: Sean Sundberg <seansund@us.ibm.com>

--- test/stages/stage1-vpc-ssh.tf
+++ test/stages/stage1-vpc-ssh.tf
@@ -4,13 +4,6 @@ module "vpcssh" {
   resource_group_name = module.resource_group.name
   region              = var.region
   name_prefix         = var.name_prefix
-  ibmcloud_api_key    = var.ibmcloud_api_key
   public_key          = ""
   private_key         = ""
-}
-
-resource null_resource write_private_key {
-  provisioner "local-exec" {
-    command = "echo '${module.vpcssh.private_key}' > .private-key && chmod 700 .private-key"
-  }
 }
