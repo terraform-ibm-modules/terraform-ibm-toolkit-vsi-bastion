1aaae1b23438347fc7e474f62d8ce8239a600906
balasgit <bala@us.ibm.com>
2022-09-01 16:43:36 -0500

    updated image

--- variables.tf
+++ variables.tf
@@ -27,7 +27,7 @@ variable "label" {
 variable "image_name" {
   type        = string
   description = "The name of the image to use for the virtual server"
-  default     = "ibm-ubuntu-22-04-6-minimal-amd64-1"
+  default     = "ibm-ubuntu-22-04-minimal-amd64-1"
 }
 
 variable "vpc_subnet_count" {
