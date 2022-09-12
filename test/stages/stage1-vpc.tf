b8c70688be9fe9fcafb374540bb3c0353b7624d3
balasgit <bala@us.ibm.com>
2022-09-12 10:56:30 -0500

    Added random prefix to VPC Name
    Signed-off-by: balasgit <bala@us.ibm.com>

--- test/stages/stage1-vpc.tf
+++ test/stages/stage1-vpc.tf
@@ -3,5 +3,12 @@ module "vpc" {
 
   resource_group_name = module.resource_group.name
   region              = var.region
-  name_prefix         = var.name_prefix
+  #name_prefix         = var.name_prefix
+  name_prefix = "${random_string.suffix.result}"
+}
+
+resource "random_string" "suffix" {
+  length           = 16
+  special          = true
+  override_special = "/@£$"
 }
