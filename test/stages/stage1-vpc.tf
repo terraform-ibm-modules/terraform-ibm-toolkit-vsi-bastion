7de5efcbf214abf5befce785fb071991654a743b
balasgit <bala@us.ibm.com>
2022-09-14 12:06:28 -0500

    added random string

--- test/stages/stage1-vpc.tf
+++ test/stages/stage1-vpc.tf
@@ -4,7 +4,7 @@ module "vpc" {
   resource_group_name = module.resource_group.name
   region              = var.region
   #name_prefix         = var.name_prefix
-  name_prefix = "${random_string.suffix.result}"
+  name_prefix         = local.name_prefix
 }
 
 resource "random_string" "suffix" {
